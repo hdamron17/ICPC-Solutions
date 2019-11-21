0aefb582805e01703db559f8347a7923e3d3a9dd
Hunter Damron <hdamron1594@yahoo.com>
2019-11-21 15:59:39 -0500

    Added tests for carryless square root problem 2019

---
+++ 2019/carryless/test.sh
@@ -0,0 +1,13 @@
+set -e
+
+inputs=$(find data -name "*.in")
+
+for ifile in $inputs; do
+  base=${ifile%.in}
+  expfile=$base.ans
+  actfile=$base.out
+  python carryless.py < $ifile > $actfile
+  if ! diff $actfile $expfile; then
+    echo "Test $base failed"
+  fi
+done
