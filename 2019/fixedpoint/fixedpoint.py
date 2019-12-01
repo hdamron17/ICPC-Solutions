279ad58a702208739319a03c5ba7379363012091
Hunter Damron <hdamron1594@yahoo.com>
2019-11-30 23:28:42 -0500

    First draft solution to fixedpoint

--- 2019/fixedpoint/fixedpoint.py
+++ 2019/fixedpoint/fixedpoint.py
@@ -5,18 +5,13 @@
 # Notes: Uses dynamic programming approach to count number of permutations for
 #        each choice of fixed points then chooses appropriate fixed points and
 #        finds permutation of remaining points.
-#
-#        Algorithm made false assumption that fixed points are ordered
-#        independently from subpermutations but this fails for
-#        x=12435 and y=12543 because the y comes first in the way of fixed
-#        points but x comes first overall
+#        Worst case for counting problem is O(n^3) and worst case for solving
+#        final answer is O(n^2)
 
 from itertools import permutations, combinations
 
-DBG = False
-dbg = lambda *s, **k: print(*s, **k) if DBG else None
-
 n, m, k = (int(x) for x in input().split())
+k -= 1  # We are indexing from 0
 k_max = 10**18+1
 
 def all_m_fixedpoint(n,m=0,k=0):
@@ -33,10 +28,11 @@ fixedpoint_table = [[[None] * (n+1) for _ in range(n+1)] for _ in range(n+1)]
 def dp_count_fixedpoint(n, m, k=0, prefix=""):
     cached_result = fixedpoint_table[n][m][k]
     if cached_result is not None:
-        # print(prefix + ":f(%d,%d,%d) = %d" % (n,m,k,cached_result))
         return cached_result
 
-    if n == 1:
+    if n == 0 and m == 0 and k == 0:
+        ret = 1  # One way to do this vacuously
+    elif n == 1:
         if m == 1 and k == 0 or m == 0 and k >= 1:
             ret = 1
         else:
@@ -57,5 +53,40 @@ def dp_count_fixedpoint(n, m, k=0, prefix=""):
     if ret > k_max:
         ret = k_max  # Not super necessary but avoids annoyingly large numbers
     fixedpoint_table[n][m][k] = ret
-    # print(prefix + " f(%d,%d,%d) = %d" % (n,m,k,ret))
     return ret
+
+answer = [None] * n
+remainder = list(range(1,n+1))
+for i in range(1,n+1):
+    free_points = len(list(filter(lambda x: x < i, remainder)))
+    sub_n = len(remainder) - 1
+    for d in remainder:
+        if d < i:
+            subcount = dp_count_fixedpoint(sub_n, m, free_points-1)
+            if k < subcount:
+                answer[i-1] = d
+                remainder.remove(d)
+                break
+            else:
+                k -= subcount
+        elif d == i:
+            subcount = dp_count_fixedpoint(sub_n, m-1, free_points)
+            if k < subcount:
+                answer[i-1] = d
+                remainder.remove(d)
+                m -= 1
+                break
+            else:
+                k -= subcount
+        else:
+            subcount = dp_count_fixedpoint(sub_n, m, free_points+1)
+            if k < subcount:
+                answer[i-1] = d
+                remainder.remove(d)
+                break
+            else:
+                k -= subcount
+    if answer[i-1] is None:
+        print(-1)
+        exit()
+print(" ".join(str(x) for x in answer))
