67d6ab7d46857014448e97044baada8c99cd7b28
Hunter Damron <hdamron1594@yahoo.com>
2019-12-01 10:40:20 -0500

    Solved fixedpoint 2019

--- 2019/fixedpoint/fixedpoint.py
+++ 2019/fixedpoint/fixedpoint.py
@@ -1,14 +1,14 @@
 # Fixed Point Permutation problem 2019
 # Created by: Hunter Damron
 # Points awarded: no
-# Status: incomplete
+# Status: complete
 # Notes: Uses dynamic programming approach to count number of permutations for
-#        each choice of fixed points then chooses appropriate fixed points and
-#        finds permutation of remaining points.
+#        each choice of fixed points then chooses each digit incrementally
+#        by counting how many correspond to each possible digit
 #        Worst case for counting problem is O(n^3) and worst case for solving
 #        final answer is O(n^2)
 
-from itertools import permutations, combinations
+from itertools import permutations
 
 n, m, k = (int(x) for x in input().split())
 k -= 1  # We are indexing from 0
@@ -30,7 +30,9 @@ def dp_count_fixedpoint(n, m, k=0, prefix=""):
     if cached_result is not None:
         return cached_result
 
-    if n == 0 and m == 0 and k == 0:
+    if n < 0 or m < 0 or k < 0:
+        ret = 0  # Shouldn't be possible but may come up
+    elif n == 0 and m == 0 and k == 0:
         ret = 1  # One way to do this vacuously
     elif n == 1:
         if m == 1 and k == 0 or m == 0 and k >= 1:
@@ -55,38 +57,49 @@ def dp_count_fixedpoint(n, m, k=0, prefix=""):
     fixedpoint_table[n][m][k] = ret
     return ret
 
-answer = [None] * n
-remainder = list(range(1,n+1))
-for i in range(1,n+1):
-    free_points = len(list(filter(lambda x: x < i, remainder)))
-    sub_n = len(remainder) - 1
-    for d in remainder:
-        if d < i:
-            subcount = dp_count_fixedpoint(sub_n, m, free_points-1)
-            if k < subcount:
-                answer[i-1] = d
-                remainder.remove(d)
-                break
-            else:
-                k -= subcount
-        elif d == i:
-            subcount = dp_count_fixedpoint(sub_n, m-1, free_points)
-            if k < subcount:
-                answer[i-1] = d
-                remainder.remove(d)
-                m -= 1
-                break
-            else:
-                k -= subcount
-        else:
-            subcount = dp_count_fixedpoint(sub_n, m, free_points+1)
-            if k < subcount:
-                answer[i-1] = d
-                remainder.remove(d)
-                break
-            else:
-                k -= subcount
-    if answer[i-1] is None:
-        print(-1)
-        exit()
-print(" ".join(str(x) for x in answer))
+def get_kth_fixedpoint_permutation(n,m,k):
+    total_count = dp_count_fixedpoint(n,m)
+    if k >= total_count:
+        return None
+
+    answer = [None] * n
+    remainder = list(range(1,n+1))
+    for i in range(1,n):
+        sub_n = len(remainder) - 1
+        free_points = sum(p < i+1 for p in remainder)
+        for d in remainder:
+            if d < i:
+                subcount = dp_count_fixedpoint(sub_n, m, free_points-1)
+                if k < subcount:
+                    answer[i-1] = d
+                    remainder.remove(d)
+                    break
+                else:
+                    k -= subcount
+            elif d == i and m > 0:
+                subcount = dp_count_fixedpoint(sub_n, m-1, free_points-1)
+                if k < subcount:
+                    answer[i-1] = d
+                    remainder.remove(d)
+                    m -= 1
+                    break
+                else:
+                    k -= subcount
+            elif d > i:
+                subcount = dp_count_fixedpoint(sub_n, m, free_points)
+                if k < subcount:
+                    answer[i-1] = d
+                    remainder.remove(d)
+                    break
+                else:
+                    k -= subcount
+        if answer[i-1] is None:
+            print("ERR: failed k=%s at digit %s: %s" % (k,i,answer))
+            break
+    if len(remainder) > 1:
+        print("ERR: not all numbers determined")
+    answer[n-1] = remainder[0]
+    return answer
+
+fmt = lambda l: " ".join(str(x) for x in l) if l is not None else -1
+print(fmt(get_kth_fixedpoint_permutation(n,m,k)))
